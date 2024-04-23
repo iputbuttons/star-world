377436409a43198f050ba7754b87984c8856b390
Francisco Rodriguez <fjrodriguez.353@gmail.com>
2024-04-23 07:31:55 -0300

    feat: improve Person screen

--- src/app/(details)/person/[personId].tsx
+++ src/app/(details)/person/[personId].tsx
@@ -1,11 +1,13 @@
+import { Button } from '@components/atoms/button'
 import { PageError } from '@components/molecules/pageError'
 import { PageLoader } from '@components/molecules/pageLoader'
-import { useLocalSearchParams } from 'expo-router'
+import { useLocalSearchParams, useRouter } from 'expo-router'
 import { Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { usePerson } from 'services/swapi/people/people.hook'
 
 export default function Person() {
+  const { back } = useRouter()
   const { personId } = useLocalSearchParams()
   const {
     data: person,
@@ -20,10 +22,22 @@ export default function Person() {
 
   return (
     <SafeAreaView style={{ backgroundColor: 'black' }}>
-      <View className='h-full w-full p-8'>
-        <Text className='text-white'>Name: {person?.name}</Text>
-        <Text className='text-white'>Height: {person?.height}</Text>
-        <Text className='text-white'>Mass: {person?.mass}</Text>
+      <View className='h-full w-full p-8 gap-8'>
+        <View className='flex-1 p-8 gap-8'>
+          <Text className='font-starjedi text-white text-2xl'>
+            {person?.name}
+          </Text>
+          <View className='gap-4'>
+            <Text className='text-white tex-base'>
+              Height: {person?.height}
+            </Text>
+            <Text className='text-white tex-base'>Mass: {person?.mass}</Text>
+            <Text className='text-white tex-base'>
+              Hair color: {person?.hair_color}
+            </Text>
+          </View>
+        </View>
+        <Button onPress={back}>Volver</Button>
       </View>
     </SafeAreaView>
   )
